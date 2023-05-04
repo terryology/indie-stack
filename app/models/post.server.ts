5b74b778ab9d5c8bffd1900c6cfdd8827d043ddb
terryology <terryyoungkin@gmail.com>
2023-05-04 04:28:27 +0000

    Fix last commit

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,5 +1,17 @@
-import { prisma } from "~/db.server";
-
-export async function getPosts() {
-  return prisma.post.findMany();
-}
+type Post = {
+    slug: string;
+    title: string;
+  };
+  
+  export async function getPosts(): Promise<Array<Post>> {
+    return [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ];
+  }
